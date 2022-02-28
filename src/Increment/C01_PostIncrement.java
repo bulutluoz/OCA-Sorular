424b1562feedac9a9302bdef86ec0deae2c91276
bulutluoz <abbulutluoz@gmail.com>
2022-02-28 15:53:07 +0100

    ilk soru

---
+++ src/Increment/C01_PostIncrement.java
@@ -0,0 +1,24 @@
+package Increment;
+
+public class C01_PostIncrement {
+
+    public static void main(String[] args) {
+
+        int num1=9;
+
+        int num2=num1++;
+
+        if(num2 < 10){
+            System.out.println(num2 + " Hello World");
+        } else {
+            System.out.println(num1 + " Hello Universel");
+        }
+
+
+    }
+
+
+
+
+
+}
