f9a88a296ccdae0c1cdf5a9c3b4b3e7be1d5d8be
bulutluoz <abbulutluoz@gmail.com>
2022-03-02 12:49:53 +0100

    2.gun soru

--- src/s02_ternary/Ternary.java
+++ src/s02_ternary/Ternary.java
@@ -8,5 +8,17 @@ public class Ternary {
         System.out.println(x>2 ? x <4 ? 10 : 8 : 7 );
 
 
+        // verilen bir sayinin tek veya cift oldugunu yazdiran bir kod olusturalim
+
+        int sayi= 20;
+        if (sayi%2==0){
+            System.out.println("sayi cift bir sayidir");
+        } else {
+            System.out.println("Sayi tek bir sayidir");
+        }
+
+        System.out.println(sayi%2==0 ? "Sayi Cift" : "Sayi Tek");
+
+
     }
 }
