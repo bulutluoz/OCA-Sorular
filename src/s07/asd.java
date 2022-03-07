cdbf97c9ed84b9083d149c782ce0e1b854e389b7
bulutluoz <abbulutluoz@gmail.com>
2022-03-07 22:30:42 +0100

    7.gun soru

---
+++ src/s07/asd.java
@@ -0,0 +1,13 @@
+package s07;
+
+public class asd {
+    public static void main(String[] args) {
+
+        byte a = 40, b = 50;
+        //byte sum =  (byte) a + b ;
+
+
+
+        //System.out.println(sum);
+    }
+}
