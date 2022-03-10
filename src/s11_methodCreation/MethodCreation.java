3eddf75940e72a9ce75d1422b4d9547fd2eaf786
bulutluoz <abbulutluoz@gmail.com>
2022-03-10 22:31:07 +0100

    11.soru

---
+++ src/s11_methodCreation/MethodCreation.java
@@ -0,0 +1,23 @@
+package s11_methodCreation;
+
+public class MethodCreation {
+
+        public static long square(int x) {
+            long y = x * (long) x;
+            x = -1;
+            return y;
+            }
+        public static void main(String[] args) {
+            int value = 9;
+            long result = square(value);
+            System.out.println(value);
+            }
+    }
+    /*
+        A. -1
+        B. 9
+        C. 81
+        D. 12. Satirdan dolayi Compile error olusur.
+        E. Baska bir satirdan dolayi Compile error olusur.
+     */
+
