e456b5589da7a0ecf4eb7c60f36c672da9e72f20
bulutluoz <abbulutluoz@gmail.com>
2022-03-14 21:56:16 +0100

    19.soru

---
+++ src/s18_ForLoopContinueKullanimi/ForLoopContinueKullanimi.java
@@ -0,0 +1,22 @@
+package s18_ForLoopContinueKullanimi;
+
+public class ForLoopContinueKullanimi {
+    public static void main(String[] args) {
+        int count = 0;
+        ROW_LOOP: for(int row = 1; row <=3; row++)
+                for(int col = 1; col <=2 ; col++) {
+                if(row * col % 2 == 0) continue ROW_LOOP;
+                count++;
+            }
+        System.out.println(count);
+    }
+}
+
+            /*
+            A. 1
+            B. 2
+            C. 3
+            D. 4
+            E. 6
+            F. 8. Satirdan dolayi kod calismaz.
+             */
---
+++ src/s19_Arrays/Arrays01.java
@@ -0,0 +1,22 @@
+package s19_Arrays;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+        int sayi=10;
+
+        sayi =20;
+
+        VAR_SAYI:for (int i = 0; i <10 ; i++)
+
+        for (int j = 10; j <20 ; j++) {
+            System.out.println("hello11");
+            sayi++;
+            System.out.println(sayi);
+            if (sayi>25) continue VAR_SAYI;
+        }
+        System.out.println("hello");
+
+
+
+    }
+}
